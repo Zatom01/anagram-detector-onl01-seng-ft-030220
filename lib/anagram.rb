95813f25e6a0e8c9f49c6d52b3f49e334c3809e1
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-13 00:26:18 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,31 +1,14 @@
-
 class Anagram
+
   attr_accessor :word
 
   def initialize(word)
-    @word =word
-  end 
-
-  def match(angaram_argu)
-    letter_arr=[]
-    final_sorted_word=[]
-    match_arr=[]
-
-    splitted_word=@word.chars.sort
-    final_sorted_word << splitted_word.join
-
-    # puts"#{final_sorted_word}"
-
-    angaram_argu.each do |ele|
-      letter_arr << ele.split("").sort.join
-      if final_sorted_word==letter_arr
-        return match_arr << ele 
-      else 
-        return letter_arr=[]
-      end
-        
-    end
-    
-   
+    @word = word
   end
-end 
+
+  def match(anagram_sentence)
+    anagram_sentence.select do |element|
+      element.split("").sort == @word.split("").sort
+    end
+  end
+end
